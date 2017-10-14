ce6ac389ffddaf68b9a0d7133f040a9fa26b6015
Renu Parameswaran <renu.parameswaran@gmail.com>
2017-10-14 16:33:07 -0700

    Update demo1.sh to update my running ec2 instance
    Update demo1.sh to integrate to my ec2 instance

--- demo1.sh
+++ demo1.sh
@@ -8,4 +8,4 @@ aws ec2 describe-instances --output table
 aws ec2 describe-instances --output json
 
 #Create tag and assign it to running instance
-aws ec2 create-tags --resources i-0b5c3557abf31fc2e --tags Key=Environment,Value=Production
+aws ec2 create-tags --resources i-0b5c1557abf389c27 --tags Key=Environment,Value=Production
